97e0d0f17785558af919c2c1758d3a2766cfa7fa
baobaohuang <huangnf95@gmail.com>
2015-06-10 10:41:23 +0800

    CreateExcel
    CreateExcel

--- CreateExcel/Properties/AssemblyInfo.cs
+++ CreateExcel/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("ConsoleApplication9")]
+[assembly: AssemblyTitle("CreateExcel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication9")]
+[assembly: AssemblyProduct("CreateExcel")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
